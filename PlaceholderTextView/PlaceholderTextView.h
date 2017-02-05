ea3db03c8717ee5e237f86b78516de22984df69c
yusayusa <yusakzk@gmail.com>
2017-02-06 01:45:59 +0900

    Initial Commit

---
+++ PlaceholderTextView/PlaceholderTextView.h
@@ -0,0 +1,19 @@
+//
+//  PlaceholderTextView.h
+//  PlaceholderTextView
+//
+//  Created by KazukiYusa on 2017/02/06.
+//  Copyright © 2017年 KazukiYusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaceholderTextView.
+FOUNDATION_EXPORT double PlaceholderTextViewVersionNumber;
+
+//! Project version string for PlaceholderTextView.
+FOUNDATION_EXPORT const unsigned char PlaceholderTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaceholderTextView/PublicHeader.h>
+
+
